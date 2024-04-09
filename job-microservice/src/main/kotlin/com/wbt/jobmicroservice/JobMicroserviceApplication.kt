3ce1dade14f135cbf74e5d22f0e1cae164d1dd1c
leonel ka <waboleonel@gmail.com>
2024-04-09 10:33:53 +0100

    Finish configure services discovery

--- job-microservice/src/main/kotlin/com/wbt/jobmicroservice/JobMicroserviceApplication.kt
+++ job-microservice/src/main/kotlin/com/wbt/jobmicroservice/JobMicroserviceApplication.kt
@@ -2,8 +2,10 @@ package com.wbt.jobmicroservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class JobMicroserviceApplication
 
 fun main(args: Array<String>) {
