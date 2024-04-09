c69f7113216184a26ba3af6d1b3418d356e9fb9b
leonel ka <waboleonel@gmail.com>
2024-04-09 10:01:02 +0100

    Add and configure eureka client to services

--- review-microservice/src/main/java/com/wbt/reviewmicroservice/ReviewMicroserviceApplication.java
+++ review-microservice/src/main/java/com/wbt/reviewmicroservice/ReviewMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.wbt.reviewmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewMicroserviceApplication {
 
 	public static void main(String[] args) {
